edfc5bc41b718964aec71922282d886594c51bbc
jonhaul <predatory2k@gmail.com>
2023-01-30 15:48:13 -0600

    backend live

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
-import os
+# import os
 
 app = Flask(__name__)
 CORS(app)
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+# basedir = os.path.abspath(os.path.dirname(__file__))
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ocrtltiumojrsv:907f40e2749ef444270accfb2dc5390c204a74e64f543e022e9719a67d29a8cc@ec2-34-231-63-30.compute-1.amazonaws.com:5432/d406s5j0hs462c'
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
